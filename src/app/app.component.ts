88eef138d78b968bf72d9cbcbb2931d19626d0c5
Jennifer Intriago <jenniferintriago28@outlook.es>
2020-11-24 21:37:22 -0500

    fisrt-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'store';
+
+  items = ['nicolas', 'jenn', 'any'];
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number){
+    this.items.splice(index, 1);
+  }
 }
